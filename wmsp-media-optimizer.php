61e9662bb6a5a631beeaaea88bb1b9d5655c1344
khushbooyd-coder <khushbooyd@gmail.com>
2026-06-27 23:32:59 +0530

    Update wmsp-media-optimizer.php

--- wmsp-media-optimizer.php
+++ wmsp-media-optimizer.php
@@ -6,8 +6,7 @@
  * Version:           1.4.0
  * Requires at least: 5.8
  * Requires PHP:      7.4
- * Author:            The Herb Company
- * Author URI:        https://theherbcompany.co.in
+ * Author:           khushboo
  * License:           GPL-2.0+
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       wmsp-media-optimizer
